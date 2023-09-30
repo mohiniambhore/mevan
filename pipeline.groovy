7f87bf0a323e7bb5e3ef145c9215a51167a19c9d
mohini <mohiniambhore26@gmail.com>
2023-09-30 23:13:04 +0530

    script

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,1 +1,30 @@
-hi mohini
+pipeline {
+    agent any
+    stages{
+        stage('pull'){
+            steps{
+                echo "hello pull complited"
+                
+            }
+            
+        }
+        stage('build'){
+            steps{
+                 echo "hello build complited"
+            }
+           
+        }
+        stage('test'){
+            steps{
+                echo "hello test complited"
+            }
+            
+        }
+        stage('deloy'){
+            steps{
+                echo "hello deploy complited"
+            }
+           
+        }
+    }
+}
